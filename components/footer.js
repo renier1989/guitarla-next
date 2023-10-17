17d2a30ddd06cfda52fec2b415fd87e190f04008
Renier Vargas <renier1989@gmail.com>
2023-10-16 21:07:06 -0500

    Class4 min Reproducir 252. Creando el Footer

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,17 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
-    <div>footer</div>
-  )
+    <footer>
+      <div className="contenedor">
+        <nav className="navegacion">
+          <Link href="/">Inicio</Link>
+          <Link href="/nosotros">Nosotros</Link>
+          <Link href="/blog">Blog</Link>
+          <Link href="/tienda">Tienda</Link>
+        </nav>
+        <p>Todos los derechos reservados</p>
+      </div>
+    </footer>
+  );
 }
