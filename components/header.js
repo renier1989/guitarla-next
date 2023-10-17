d4fe5b62f475da92ebf0ab616d067dc2dcd421de
Renier Vargas <renier1989@gmail.com>
2023-10-16 20:54:20 -0500

    Class250. Finalizando el CSS del Header

--- components/header.js
+++ components/header.js
@@ -1,19 +1,24 @@
 import Image from "next/image";
 import Link from "next/link";
-import styles from "../styles/header.module.css"
+import styles from "../styles/header.module.css";
 
 export default function Header() {
-    return (
-      <header className={styles.header}>
-        <div className="contenedor">
-            <Image src="/img/logo.svg" width={300} height={40} alt="imagen logotipo" />
-        </div>
+  return (
+    <header className={styles.header}>
+      <div className={`contenedor ${styles.barra}`}>
+        <Image
+          src="/img/logo.svg"
+          width={300}
+          height={40}
+          alt="imagen logotipo"
+        />
         <nav className={styles.navegacion}>
-            <Link href="/">Inicio</Link>
-            <Link href="/nosotros">Nosotros</Link>
-            <Link href="/blog">Blog</Link>
-            <Link href="/tienda">Tienda</Link>
+          <Link href="/"> Inicio </Link>
+          <Link href="/nosotros">Nosotros</Link>
+          <Link href="/blog">Blog</Link>
+          <Link href="/tienda">Tienda</Link>
         </nav>
-      </header>
-    )
-  }
+      </div>
+    </header>
+  );
+}
