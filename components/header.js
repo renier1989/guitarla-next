d0474c6101144af05eb78b366009a2d7e1e04cd1
Renier Vargas <renier1989@gmail.com>
2023-10-16 21:01:05 -0500

    Class251. Resaltar la página actual

--- components/header.js
+++ components/header.js
@@ -1,22 +1,26 @@
 import Image from "next/image";
 import Link from "next/link";
 import styles from "../styles/header.module.css";
+import { useRouter } from "next/router";
 
 export default function Header() {
+    const router = useRouter()
   return (
     <header className={styles.header}>
       <div className={`contenedor ${styles.barra}`}>
+        <Link href="/">
         <Image
           src="/img/logo.svg"
           width={300}
           height={40}
           alt="imagen logotipo"
         />
+        </Link>
         <nav className={styles.navegacion}>
-          <Link href="/"> Inicio </Link>
-          <Link href="/nosotros">Nosotros</Link>
-          <Link href="/blog">Blog</Link>
-          <Link href="/tienda">Tienda</Link>
+          <Link href="/" className={router.pathname === '/' ? styles.active : '' }> Inicio </Link>
+          <Link href="/nosotros" className={router.pathname === '/nosotros' ? styles.active : '' }>Nosotros</Link>
+          <Link href="/blog" className={router.pathname === '/blog' ? styles.active : '' }>Blog</Link>
+          <Link href="/tienda" className={router.pathname === '/tienda' ? styles.active : '' }>Tienda</Link>
         </nav>
       </div>
     </header>
