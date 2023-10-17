759a523b5970981d62d28b4089062a457f62f059
Renier Vargas <renier1989@gmail.com>
2023-10-16 19:24:41 -0500

    Class244. El Modelo basado en Rutas y Link en Next.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
-      
-
       <h1>Hola, esta es una aplicacion de Next</h1>
+      <Link href="/nosotros">Ir a nosotros</Link>
     </>
   );
 }
---
+++ pages/nosotros.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Nosotros() {
+  return <div>
+
+    <Link href='/'>Ir al inicio</Link>
+
+
+  </div>;
+}
