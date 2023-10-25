9338f6566bcc823f70428a25f5d1989fa54baeba
Renier Vargas <renier1989@gmail.com>
2023-10-24 19:54:51 -0500

    Class256. Consultar la API con getStaticProps

--- pages/tienda.js
+++ pages/tienda.js
@@ -1,7 +1,8 @@
 import Layout from "@/components/layout";
 import ListadoGuitarras from "@/components/listado-guitarras";
 
-export default function Tienda() {
+export default function Tienda({guitarras}) {
+  console.log(guitarras);
   return (
     <Layout
       title={"Tienda Virtual"}
@@ -16,4 +17,18 @@ export default function Tienda() {
       </main>
     </Layout>
   );
+}
+
+export async function getStaticProps(){
+
+  const resp = await fetch(`${process.env.API_URL}/guitarras?populate=imagen`);
+  const {data: guitarras} = await resp.json();
+
+  // console.log(guitarras);
+
+  return {
+    props:{
+      guitarras
+    }
+  }
 }
