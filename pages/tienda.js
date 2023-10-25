9b3fab4fd7a678d97269acd31dd4644739c04dda
Renier Vargas <renier1989@gmail.com>
2023-10-24 20:13:51 -0500

    ClassDetener 257. Consultar la API con getServerSideProps

--- pages/tienda.js
+++ pages/tienda.js
@@ -19,13 +19,23 @@ export default function Tienda({guitarras}) {
   );
 }
 
-export async function getStaticProps(){
+// // esta funcion simepre requiere de un build por cada cambio en la api
+// export async function getStaticProps(){
+//   const resp = await fetch(`${process.env.API_URL}/guitarras?populate=imagen`);
+//   const {data: guitarras} = await resp.json();
+//   // console.log(guitarras);
+//   return {
+//     props:{
+//       guitarras
+//     }
+//   }
+// }
 
+
+export async function getServerSideProps(){
   const resp = await fetch(`${process.env.API_URL}/guitarras?populate=imagen`);
   const {data: guitarras} = await resp.json();
-
   // console.log(guitarras);
-
   return {
     props:{
       guitarras
