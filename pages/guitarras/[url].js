15ab8b86a2836dec36ab341d3977e04a839ff368
Renier Vargas <renier1989@gmail.com>
2023-10-26 21:31:36 -0500

    Class261. Routing Dinamico con getServerSideProps

--- pages/guitarras/[url].js
+++ pages/guitarras/[url].js
@@ -1,6 +1,20 @@
 
-export default function Guitarra() {
+export default function Guitarra({guitarra}) {
+  console.log(guitarra);
   return (
     <div>Guitarra unica</div>
   )
+}
+
+export async function getServerSideProps({query: {url}}){
+  
+  const respuesta = await fetch(`${process.env.API_URL}/guitarras?filters[url]=${url}&populate=imagen`);
+  const {data: guitarra} = await respuesta.json();
+
+
+  return{
+    props:{
+      guitarra
+    }
+  }
 }
