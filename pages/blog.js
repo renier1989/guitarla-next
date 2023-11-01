c7fc3a1ca87e58617b3560a30a711090ba046f87
Renier Vargas <renier1989@gmail.com>
2023-11-01 10:09:22 -0500

    Class265. Consultando los Posts

--- pages/blog.js
+++ pages/blog.js
@@ -1,12 +1,36 @@
 import Layout from "@/components/layout";
+import Post from "@/components/post";
 
-export default function Blog() {
+export default function Blog({posts}) {
+  
   return (
     <Layout
       title={"Blog"}
       description={"Blog de música, consejos musicales, noticas de guitarras, GuitarLA"}
     >
-      <h1>Blog</h1>
+      <main className="contenedor">
+        <h2 className="heading">Blog</h2>
+        <div>
+          {posts?.map(post=>(
+            <Post 
+            key={post.id}
+            post={post.attributes}
+            />
+          ))}
+        </div>
+      </main>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const respuesta = await fetch(
+    `${process.env.API_URL}/posts?populate=imagen`
+  );
+  const { data: posts } = await respuesta.json();
+  return {
+    props: {
+      posts,
+    },
+  };
 }
