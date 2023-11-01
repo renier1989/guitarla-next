e432694fde1a37a7f8cdac80750908a0e630ad00
Renier Vargas <renier1989@gmail.com>
2023-11-01 18:24:07 -0500

    Class281. React 18, Local Storage, Doble render y Next.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import "@/styles/globals.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function App({ Component, pageProps }) {
-  const [carrito, setCarrito] = useState([]);
+
+  const carritoLS = typeof window !== "undefined" ? JSON.parse(localStorage.getItem('carrito')) ?? [] : [];
+  const [carrito, setCarrito] = useState(carritoLS);
+  useEffect( () => {
+    localStorage.setItem('carrito', JSON.stringify(carrito))
+  }, [carrito] );
+
 
   const agregarCarrito = (guitarra) => {
     // Comprobar si la guitarra ya esta en el carrito...
