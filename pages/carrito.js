e2d597854fa8c8d9699b68df41ad30dd1fb76c19
Renier Vargas <renier1989@gmail.com>
2023-11-01 14:55:09 -0500

    Class278. Agregando un Select para Modificar las Cantidades

--- pages/carrito.js
+++ pages/carrito.js
@@ -2,7 +2,7 @@ import Layout from "@/components/layout";
 import styles from "../styles/carrito.module.css";
 import Image from "next/image";
 
-export default function Carrito({ carrito }) {
+export default function Carrito({ carrito, actualizarCantidad }) {
   return (
     <Layout title="Carrito de Compras">
       <main className="contenedor">
@@ -25,11 +25,31 @@ export default function Carrito({ carrito }) {
                     </div>
                     <div>
                       <p className={styles.nombre}>{producto.nombre}</p>
-                      <p>Cantidad : {producto.cantidad}</p>
-                      <p className={styles.precio}>$<span>{producto.precio}</span></p>
+                      <div className={styles.cantidad}>
+                        <p>Cantidad: </p>
+                        <select
+                          className={styles.select}
+                          onChange={e =>
+                            actualizarCantidad({
+                              id: producto.id,
+                              cantidad: e.target.value,
+                            })
+                          }
+                          value={producto.cantidad}
+                        >
+                          <option value="1">1</option>
+                          <option value="2">2</option>
+                          <option value="3">3</option>
+                          <option value="4">4</option>
+                          <option value="5">5</option>
+                        </select>
+                      </div>
+                      <p className={styles.precio}>
+                        $<span>{producto.precio}</span>
+                      </p>
                       <p className={styles.subtotal}>
-                        
-                        Subtotal: $<span>{producto.cantidad * producto.precio}</span>
+                        Subtotal: $
+                        <span>{producto.cantidad * producto.precio}</span>
                       </p>
                     </div>
                   </div>
