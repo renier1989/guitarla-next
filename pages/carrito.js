b8b14d95d683d7323502106cadbeb39325fdfe14
Renier Vargas <renier1989@gmail.com>
2023-11-01 13:08:43 -0500

    Class276. Mostrando el Contenido del carrito

--- pages/carrito.js
+++ pages/carrito.js
@@ -1,23 +1,36 @@
 import Layout from "@/components/layout";
-import styles from "../styles/carrito.module.css"
+import styles from "../styles/carrito.module.css";
+import Image from "next/image";
 
-export default function Carrito() {
+export default function Carrito({ carrito }) {
   return (
-    <Layout 
-        title="Carrito de Compras"
-    >
-        <main className="contenedor">
+    <Layout title="Carrito de Compras">
+      <main className="contenedor">
         <h1 className="heading">Carrito</h1>
         <div className={styles.contenido}>
-            <div className={styles.carrito}>
-                <h2>Artículos</h2>
-            </div>
-            <aside className={styles.resumen}>
-                <h3>Resumen del pedido</h3>
-                <p>Total a pagar: $1000</p>
-            </aside>
+          <div className={styles.carrito}>
+            <h2>Artículos</h2>
+
+            {carrito.length === 0
+              ? "Carrito Vacio"
+              : carrito.map((producto) => <div key={producto.id} className={styles.producto}> 
+                    <div>
+                        <Image width={250} height={480} src={producto.imagen} alt={`Imagen de la guitarra ${producto.nombre}`} />
+                    </div>
+                    <div>
+                        <p className={styles.nombre}>{producto.nombre}</p>
+                        <p className={styles.precio}>${producto.precio}</p>
+                        <p className={styles.subtotal}> Subtotal: ${producto.cantidad * producto.precio} </p>
+                    </div>
+
+              </div>)}
+          </div>
+          <aside className={styles.resumen}>
+            <h3>Resumen del pedido</h3>
+            <p>Total a pagar: $1000</p>
+          </aside>
         </div>
-        </main>
+      </main>
     </Layout>
-  )
+  );
 }
